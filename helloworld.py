f7cdb957efe948b1d9cdef544fe59b2fb7085fa8
Sravya Sree Kollu <sravyakhanna007@gmail.com>
2024-08-22 05:28:43 +0000

    second commit

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,11 @@
 #my flask app
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World! from python flask app--Sravya"
+
+if __name__ == "__main__":
+    app.run(debug=True)
